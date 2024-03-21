9b88cd4063143355dd7921aab44a00a22d4d9faa
Zoirjon <zoirjonabduvohidov0@gmail.com>
2024-03-21 15:42:32 +0500

    drawer edited

--- src/components/Header/Drawer/index.tsx
+++ src/components/Header/Drawer/index.tsx
@@ -40,11 +40,11 @@ const Drawer = ({ open, setOpen, classNames, links }: thisProps) => {
     }
     return ReactDOM.createPortal(
         <div id='drawer'>
-            <div className={`fixed z-[-1] w-screen bottom-0 h-[100dvh] duration-500 drawer 
+            <div className={`fixed z-[-1] w-screen bottom-0 h-full duration-500 drawer 
              ${open ? 'backdrop-blur-md !z-[1000]' : 'opacity-0'}`}
             />
             <div className={`fixed shadow-card w-[300px] p-[16px] max-w-full !z-[1000] !duration-500 ml-[-20px] h-[100vh] top-0 right-0 flex  shadow-lg flex-col gap-[16px] items-center ${open ? 'translate-x-[1px] duration-400' : 'translate-x-[300px]'} bg-primary border-1 border-white rounded-l-lg py-6`}>
-                <div className={open ? 'w-full !z-[1000] flex flex-col justify-between h-full pl-12' : 'hidden'}>
+                <div className={open ? 'w-full !z-[1000] flex flex-col justify-between h-[100dvh] pl-12' : 'hidden'}>
                     <div>
                         <div className='flex justify-end'>
                             <X className='text-white cursor-pointer' onClick={() => setOpen(false)} />
